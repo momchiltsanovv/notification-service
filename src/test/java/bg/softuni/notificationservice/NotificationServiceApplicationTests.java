3e1f140d498ef1656c8688ae526b8be5dc4438db
Momchil Tsanov <momchil395@gmail.com>
2025-11-03 10:33:40 +0200

    structure

---
+++ src/test/java/bg/softuni/notificationservice/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.softuni.notificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
